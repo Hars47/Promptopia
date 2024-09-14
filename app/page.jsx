5c65f143a7eacd464e4dc4c6d3b4a0b3addcb2ce
Hars47 <saharsha2004@gmail.com>
2024-09-14 20:15:14 +0530

    basic layout, page file and created some components

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,22 @@
-const page = () => {
+const Home = () => {
   return (
-    <div>
-      
-    </div>
+    <section className="w-full flex-center flex-col">
+
+      {/* underscore here means, the styles are from globals.css */}
+
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient">AI-Powered Prompts</span>
+      </h1>
+
+        <p className="desc text-center">Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts.
+        </p>
+        
+        {/* Feed */}
+
+    </section>
   )
 }
 
-export default page
+export default Home
