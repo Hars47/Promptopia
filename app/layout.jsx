5d7d2c823d4f3a8cde1601c1900377fe729cf846
Hars47 <saharsha2004@gmail.com>
2024-09-14 21:19:38 +0530

    setting up google provider and next-auth

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,24 +3,25 @@ import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
 export const metadata = {
-    title: 'Promptopia',
-    description: 'Discover & Share AI prompts'
-}
+  title: "Promptopia",
+  description: "Discover & Share AI prompts",
+};
 const RootLayout = ({ children }) => {
-    return (
-        <html lang="en">
-            <body>
-                <div className="main">
-                    <div className="gradient" />
-                </div>
-
-                <main className="app">
-                    <Nav />
-                    {children}
-                </main>
-            </body>
-        </html>
-    )
-}
+  return (
+    <html lang="en">
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
+    </html>
+  );
+};
 
 export default RootLayout;
